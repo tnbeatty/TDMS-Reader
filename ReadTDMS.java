5ae062f1187620fead542d243f44c2a37597fc67
tnbeatty <nate.beatty@gmail.com>
2014-01-06 18:00:39 -0500

    Basic class file

---
+++ ReadTDMS.java
@@ -0,0 +1,14 @@
+
+class ReadTDMS {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to TDMS Reader!");
+
+		if (args.length != 0) {
+			System.out.println(args[0]);
+		}
+
+	}
+
+}
